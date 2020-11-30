9e2f440204f74f93bf591900cb6a5d0f44a034cf
franec94 <franec94@gmail.com>
2020-11-30 11:44:33 +0100

    Added new scheduler for pruning-aware training.

--- Makefile
+++ Makefile
@@ -8,6 +8,9 @@ RUN_PROGRAM = run_program_siren_app.sh
 TEST_RUN_PROGRAM = test_run_program_siren_app.sh
 
 
+RUN_PROGRAM_AGP_PRUNE = run_program_siren_app_pruning_agp.sh
+
+
 # ======================== #
 # Targets
 # ======================== #
@@ -21,6 +24,11 @@ run_program_siren_app_train_on_cameramen:
 	clear
 	chmod u+x $(RUN_PROGRAM_APP)
 	./$(RUN_PROGRAM_APP)
+
+run_program_siren_app_train_on_cameramen_agp_prune:
+	clear
+	chmod u+x $(RUN_PROGRAM_AGP_PRUNE)
+	./$(RUN_PROGRAM_AGP_PRUNE)
 
 test_run_program_siren_app_train_on_cameramen:
 	clear
---
+++ run_program_siren_app_pruning_agp.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# =============================================== #
+# Script: run_program.sh
+# Used it for launching a run for training 
+# a deep learning model based on Siren-like 
+# Architecture for retrieving model's weights
+# that all together represent Cameramen compressed
+# image.
+# =============================================== #
+
+CUDA_VISIBLE_DEVICES=0 python main.py \
+  --logging_root '../../../results/cameramen/distiller-siren/agp_prune' \
+  --experiment_name 'train' \
+  --sidelength 256 \
+  --num_epochs 175000 \
+  --n_hf 64  \
+  --n_hl 5 \
+  --lambda_L_1 0 \
+  --lambda_L_2 0 \
+  --epochs_til_ckpt 10000 \
+  --compress ./examples/agp-pruning/siren64-5_schedule_agp.yaml \
+  --save_mid_ckpts 99999 149999 199999 249999 299999 349999 399999 449999 499999  \
+  --save_mid_ckpts 99999 149999 \
+  --seed 0 \
+  --cuda \
+  --train \
+  --evaluate \
+  --verbose 0
+
+exit 0

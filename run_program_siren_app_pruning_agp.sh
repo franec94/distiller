13d6154d64b8e9a0a51a9a44571079ac7f26b368
franec94 <franec94@gmail.com>
2020-12-06 11:31:18 +0100

    added some code..

--- run_program_siren_app_pruning_agp.sh
+++ run_program_siren_app_pruning_agp.sh
@@ -12,21 +12,22 @@
 CUDA_VISIBLE_DEVICES=0 python main.py \
   --logging_root '../../../results/cameramen/distiller-siren/agp_prune' \
   --experiment_name 'train' \
-  --compress ./examples/agp-pruning/siren64-5_schedule_agp.yaml \
+  --compress ./examples/agp-pruning/siren64_5.schedule_agp.yaml \
   --sidelength 256 \
   --n_hf 64  \
   --n_hl 5 \
   --seed 0 \
   --cuda \
-  --num_epochs 200000 \
-  --lr 1.0 \
+  --num_epochs 450000 \
+  --lr 0.0001 \
   --lambda_L_1 0 \
   --lambda_L_2 0 \
   --epochs_til_ckpt 10000 \
   --num-best-scores 5 \
-  --save_mid_ckpts 99999 149999 174999 199999 \
   --train \
   --evaluate \
-  --verbose 0
+  --verbose 0 \
+  --resume-from "../../../ckpts/_mid_ckpt_epoch_299999.pth.tar"
 
+# --save_mid_ckpts 99999 149999 174999 199999 \
 exit 0

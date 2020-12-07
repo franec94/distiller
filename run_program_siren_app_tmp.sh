48f77b03a20feb9e354d6d21f03f2919bb686f9d
franec94 <franec94@gmail.com>
2020-12-07 19:19:49 +0100

    Added Makefile_2 for fine tuning step

--- run_program_siren_app_tmp.sh
+++ run_program_siren_app_tmp.sh
@@ -12,22 +12,24 @@
 CUDA_VISIBLE_DEVICES=0 python siren_main_app.py \
   --logging_root '../../../results/cameramen/distiller-siren/agp_prune' \
   --experiment_name 'train' \
-  --compress "../../../schedulers/agp-pruning/siren64_5.schedule_agp.yaml" \
+  --compress "../../../schedulers/fine-tuning/siren64_5.schedule_fine_tuning.yaml" \
   --sidelength 256 \
   --n_hf 64  \
   --n_hl 5 \
   --seed 0 \
   --cuda \
-  --num_epochs 450000 \
+  --num_epochs 550000 \
   --lr 0.0001 \
   --lambda_L_1 0 \
   --lambda_L_2 0 \
-  --epochs_til_ckpt 10000 \
+  --epochs_til_ckpt 750 \
   --num-best-scores 5 \
   --train \
   --evaluate \
   --verbose 0 \
-  --resume-from "../../../ckpts/_mid_ckpt_epoch_299999.pth.tar"
+  --resume-from "../../../ckpts/_final_ckpt_epoch_449999.pth.tar"
+
+#  --resume-from "../../../ckpts/_mid_ckpt_epoch_299999.pth.tar"
 
 # --compress ./examples/agp-pruning/siren64_5.schedule_agp.yaml \
 # --save_mid_ckpts 99999 149999 174999 199999 \

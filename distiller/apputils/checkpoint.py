9fcdaa6b14fef9bc9016703af8153e1083e3b8c5
franec94 <franec94@gmail.com>
2020-12-21 14:44:09 +0100

    Fix code for performance reasons.

--- distiller/apputils/checkpoint.py
+++ distiller/apputils/checkpoint.py
@@ -90,41 +90,44 @@ def save_checkpoint(epoch, arch, model, optimizer=None, scheduler=None,
     if freq_ckpt is not None and epoch >= 0 and epoch % freq_ckpt == 0:
         torch.save(checkpoint, fullpath)
     # else: torch.save(checkpoint, fullpath)
-    
+
     if is_best:
         # Save new best result into dedicated ckpt.
         filename_best = 'best.pth.tar' if name is None else name + '_best.pth.tar'
         fullpath_best = os.path.join(dir, filename_best)
         shutil.copyfile(fullpath, fullpath_best)
+
     if is_mid_ckpt:
         # Save middle desired epoch-ckpt result into dedicated ckpt.
         filename_mid_ckpt = f'mid_ckpt_epoch_{epoch}.pth.tar' if name is None else name + f'_mid_ckpt_epoch_{epoch}.pth.tar'
         fullpath_mid_ckpt = os.path.join(dir, filename_mid_ckpt)
         shutil.copyfile(fullpath, fullpath_mid_ckpt)
-    if is_last_epoch:
+    elif is_last_epoch:
         # Save last desired epoch-ckpt result into dedicated ckpt.
         filename_final_ckpt = f'final_ckpt_epoch_{epoch}.pth.tar' if name is None else name + f'_final_ckpt_epoch_{epoch}.pth.tar'
         fullpath_final_ckpt = os.path.join(dir, filename_final_ckpt)
         shutil.copyfile(fullpath, fullpath_final_ckpt)
+    
     if is_one_to_save_pruned and save_mid_pr_obj is not None:
         # Save desired epoch-ckpt result into dedicated ckpt if prunig scheduler is on.
         prune_rate = save_mid_pr_obj.get_rate_to_save()
-        if prune_rate is not None:
-            filename_prune_rate_ckpt = f'pruned_ckpt_epoch_{epoch}_prune_rate_{prune_rate}.pth.tar' if name is None else name + f'_pruned_ckpt_epoch_{epoch}_prune_rate_{prune_rate}.pth.tar'
-            fullpath_prune_rate_ckpt = os.path.join(dir, filename_prune_rate_ckpt)
-            shutil.copyfile(fullpath, fullpath_prune_rate_ckpt)
-            if prune_details is not None:
-                out_file_data = os.path.join(f'{dir}', f'data_prune_rate_{prune_rate}_details.json')
-                str_data = json.dumps(prune_details)
+        # if prune_rate is not None:
+        filename_prune_rate_ckpt = f'pruned_ckpt_epoch_{epoch}_prune_rate_{prune_rate}.pth.tar' if name is None else name + f'_pruned_ckpt_epoch_{epoch}_prune_rate_{prune_rate}.pth.tar'
+        fullpath_prune_rate_ckpt = os.path.join(dir, filename_prune_rate_ckpt)
+        shutil.copyfile(fullpath, fullpath_prune_rate_ckpt)
+        # if prune_details is not None:
+        out_file_data = os.path.join(f'{dir}', f'data_prune_rate_{prune_rate}_details.json')
+        str_data = json.dumps(prune_details)
 
-                msglogger.info(f"--- dump pruning data (epoch={epoch}) (pruning_rate={prune_rate})---------")
-                msglogger.info(f"Data saved to: {out_file_data}")
-                msglogger.info(str_data)
-                try:
-                    with open(out_file_data, 'w') as outfile:
-                        json.dump(prune_details, outfile)
-                except Exception as err:
-                    msglogger.info(f"{str(err)}.\nError occour when attempting to saving: {out_file_data}")
+        msglogger.info(f"--- dump pruning data (epoch={epoch}) (pruning_rate={prune_rate})---------")
+        msglogger.info(f"Data saved to: {out_file_data}")
+        msglogger.info(str_data)
+        try:
+            with open(out_file_data, 'w') as outfile:
+                json.dump(prune_details, outfile)
+        except Exception as err:
+            msglogger.info(f"{str(err)}.\nError occour when attempting to saving: {out_file_data}")
+
 
 
 def load_lean_checkpoint(model, chkpt_file, model_device=None):

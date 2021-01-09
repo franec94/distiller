88093a416bcca791167524d8da8a5981f89248c0
franec94 <franec94@gmail.com>
2021-01-09 16:35:23 +0100

    Save test data to csv support, to be tested.

--- src/generic/save_test_dat_to_csv.py
+++ src/generic/save_test_dat_to_csv.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 import os
 import sys
 import pandas as pd
@@ -9,10 +10,12 @@ def save_test_data_to_csv(opt, results_test, app):
 
     net_layers = list(df_model["Name"].values)
     net_layers = net_layers[0:(len(net_layers)-1)]
+    pprint(net_layers)
     # headers = []
     # for a_layer in net_layers: headers.extend([f"{a_layer}-{a_feature}" for a_feature in "ws,act,bias".split(",")])
     wts_sparse = list(df_model["NNZ (sparse)"].values)
     wts_sparse = wts_sparse[:(len(wts_sparse)-1)]
+    pprint(wts_sparse)
 
     date_train = None
     if app.logdir:
@@ -23,9 +26,13 @@ def save_test_data_to_csv(opt, results_test, app):
 
     if "quantizer" in compress_dict.keys():
         keys = list(compress_dict["quantizer"]["linear_quantizer"].keys())
+        pprint(keys)
         values = list(compress_dict["quantizer"]["linear_quantizer"].values())
+        pprint(values)
         keys_p = list(compress_dict["policies"][0].keys())[1:]
+        pprint(keys_p)
         values_p = list(compress_dict["policies"][0].values())[1:]
+        pprint(values_p)
 
         columns = "date,MSE,PSNR,SSIM,TIME".split(",") + keys + keys_p + net_layers
         a_record = [date_train] + results_test + values + values_p + wts_sparse

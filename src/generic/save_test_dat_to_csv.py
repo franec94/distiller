5b997a6845f98cebd2b9e6de70ded9ab62c177ad
franec94 <franec94@gmail.com>
2021-01-09 16:58:39 +0100

    Save test data to csv support, to be tested.

--- src/generic/save_test_dat_to_csv.py
+++ src/generic/save_test_dat_to_csv.py
@@ -10,31 +10,32 @@ def save_test_data_to_csv(opt, results_test, app):
 
     net_layers = list(df_model["Name"].values)
     net_layers = net_layers[0:(len(net_layers)-1)]
-    pprint(net_layers)
+    # pprint(net_layers)
     # headers = []
     # for a_layer in net_layers: headers.extend([f"{a_layer}-{a_feature}" for a_feature in "ws,act,bias".split(",")])
     wts_sparse = list(df_model["NNZ (sparse)"].values)
     wts_sparse = wts_sparse[:(len(wts_sparse)-1)]
-    pprint(wts_sparse)
+    # pprint(wts_sparse)
 
     date_train = None
     if app.logdir:
+        print(app.logdir)
         tmp_log_dir = os.path.normpath(app.logdir)
         date_train = os.path.dirname(tmp_log_dir)
 
     with open(opt.compress) as compress_file:
         compress_dict = yaml.load(compress_file, Loader=yaml.FullLoader)
-        pprint(compress_dict)
+        # pprint(compress_dict)
 
     if "quantizers" in compress_dict.keys():
         keys = list(compress_dict["quantizers"]["linear_quantizer"].keys())
-        pprint(keys)
+        # pprint(keys)
         values = list(compress_dict["quantizers"]["linear_quantizer"].values())
-        pprint(values)
+        # pprint(values)
         keys_p = list(compress_dict["policies"][0].keys())[1:]
-        pprint(keys_p)
+        # pprint(keys_p)
         values_p = list(compress_dict["policies"][0].values())[1:]
-        pprint(values_p)
+        # pprint(values_p)
 
         columns = "date,MSE,PSNR,SSIM,TIME".split(",") + keys + keys_p + net_layers
         a_record = [date_train] + list(results_test) + values + values_p + wts_sparse
@@ -48,5 +49,5 @@ def save_test_data_to_csv(opt, results_test, app):
         try:
             os.makedirs(dir_name)
         except: pass
-        df.to_csv(file_name)
+        df.to_csv(file_name, index_col = False)
     pass

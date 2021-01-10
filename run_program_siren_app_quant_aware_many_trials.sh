3e9df80a182b62c5a7758684758ed6023851b68f
franec94 <franec94@gmail.com>
2021-01-10 11:21:15 +0100

    Starting implementation for supporting multiple trials for quant aware training.

---
+++ run_program_siren_app_quant_aware_many_trials.sh
@@ -0,0 +1,110 @@
+#!/usr/bin/env bash
+
+# ======================================================== #
+# Script: siren_main_app.sh
+# Used it for launching a run for training 
+# a deep learning model based on Siren-like 
+# Architecture for retrieving model's weights
+# that all together represent Cameramen compressed image:
+# 
+# (1.a) By means of just a pre-trained model;
+# (1.b) By means of pre-trained and already pruned model.
+# (2) Attempting to quantize it, via yaml scheduler.
+# ======================================================== #
+
+function check_file_exists() {
+    local file_path=$1
+    echo -e "[*] check file - '{file_path}' exists..."
+    if [ ! -f "${file_path}" ] ; then
+        echo "Error: '${file_path}' is not a file."
+        exit -1
+    fi
+    echo -e "[*] check file - Done."
+}
+
+function run_trials_linear_quant() {
+    local INITIALIZED_MODEL=$1
+
+    LOGGING_ROOT='../../../results/cameramen/distiller-siren/train/quant-aware/linear-quant'
+    COMPRESS_SCHEDULE="../../../quant-configs/siren_quant_aware_train_linear_quant.yaml"
+    COMPRESS_COMBS="../../../quant-configs/quant-combs/siren_quant_aware_train_linear_quant.csv"
+
+    check_file_exists $LOGGING_ROOT
+    check_file_exists $COMPRESS_SCHEDULE
+    check_file_exists $COMPRESS_COMBS
+
+    declare -A MAP_OPTS
+    declare -A MAP_IDX_OPT
+    i=0
+    while IFS= read -r line ; do
+        # echo "$line"
+        options=$(echo $line | tr "," "\n")
+        if [ i -eq 0 ] ; then
+            j=0 
+            for opt in $options ; do
+                echo "> [$opt]"
+                MAP_OPTS[${opt}]=""
+                MAP_IDX_OPT[j]=$opt
+                j=$((j+1))
+            done
+
+        else
+            j=0 
+            for opt in $options ; do
+                echo "> [$opt]"
+                opt_name=MAP_IDX_OPT[j]
+                MAP_OPTS[$opt_name]=${opt}
+                j=$((j+1))
+            done
+            pos_comb=$((i-1))
+            python3 update_quant_scheduler.py \
+                --compress $COMPRESS_SCHEDULE \
+                --combs COMPRESS_COMBS \
+                --pos_comb $pos_comb
+            # run_trials $LOGGING_ROOT $COMPRESS_SCHEDULE $INITIALIZED_MODEL $MAP_OPTS
+        fi
+        i=$((i+1))
+    done < "$COMPRESS_COMBS"
+}
+
+function run_trials() {
+    local LOGGING_ROOT=$1
+    local COMPRESS_SCHEDULE=$2
+    local INITIALIZED_MODEL=$3
+    local MAP_OPTS=$4
+    CUDA_VISIBLE_DEVICES=0 python3 siren_main_app.py \
+        --logging_root ${LOGGING_ROOT} \
+        --experiment_name 'quant_aware_train' \
+        --sidelength 256 \
+        --n_hf 64 \
+        --n_hl 5 \
+        --seed 0 \
+        --cuda \
+        --num_epochs MAP_OPTS[epochs] \
+        --lr MAP_OPTS[lr] \
+        --verbose 0 \
+        --exp-load-weights-from ${INITIALIZED_MODEL} \
+        --compress ${COMPRESS_SCHEDULE} \
+}
+
+INITIALIZED_MODEL="../../../ckpts/_mid_ckpt_epoch_299999.pth.tar"
+
+run_trials_linear_quant $INITIALIZED_MODEL
+
+exit 0
+
+
+LOGGING_ROOT='../../../results/cameramen/distiller-siren/train/quant-aware/dorefa-quant'
+COMPRESS_SCHEDULE="../../../quant-configs/siren_quant_aware_train_linear_quant.yaml"
+
+LOGGING_ROOT='../../../results/cameramen/distiller-siren/train/quant-aware/wrpn-quant'
+COMPRESS_SCHEDULE="../../../quant-configs/siren_quant_aware_train_linear_quant.yaml"
+
+LOGGING_ROOT='../../../results/cameramen/distiller-siren/train/quant-aware/pact-quant'
+COMPRESS_SCHEDULE="../../../quant-configs/siren_quant_aware_train_linear_quant.yaml"
+
+# Option: "exp-load-weights-from":
+# (1) --exp-load-weights-from "../../../ckpts/_mid_ckpt_epoch_299999.pth.tar" \
+# (2) --exp-load-weights-from "../../../ckpts/_final_ckpt_epoch_449999.pth.tar" \
+
+exit 0
---
+++ update_quant_scheduler.py
@@ -0,0 +1,111 @@
+from pprint import pprint
+import os
+import sys
+import pandas as pd
+import yaml
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--compress", type=str, required=True, dest="compress", \
+    help="Input file path, within local file system, to resource that should be updated and contains constranints to be used for quant compression process of a DNN model."
+)
+parser.add_argument("--combs", type=str, required=True, dest="combs", \
+    help="Input file path, within local file system, to resource that contains a set of possible options for updating scheduler yaml-compliant file."
+)
+parser.add_argument("--pos_comb", type=int, required=True, dest="pos_comb", \
+    help="Input pos to comn to be exploited."
+)
+
+
+def check_file_exists(file_path: str, ext: str=None) -> None:
+    """Check wheter input file path exists.
+    Args:
+    -----
+    `file_path` - str python object that addresses input file path within local file system.\n
+    `ext` - str python object that represent file extension to be checked if it is provided, so if it is not None (default=None).\n
+    Return:
+    ------
+    `dict`-  python dict object with details of how to carry out compression proces by means of quantization trick.\n
+    """
+    if os.path.exists(file_path) is False:
+        print(f"Error: resource '{file_path}' not exists!", file=sys.stderr)
+        sys.exit(-1)
+    if os.path.isfile(file_path) is False:
+        print(f"Error: resource '{file_path}' is not a file!", file=sys.stderr)
+        sys.exit(-2)
+    if ext:
+        _, file_ext = os.path.splitext(file_path)
+        if ext != file_ext:
+            print(f"Error: resource '{file_path}' is not a {ext} file!", file=sys.stderr)
+            sys.exit(-3)
+    pass
+
+
+def read_yaml_file_to_dict(args) -> dict:
+    """Read input file content which is a yaml file like.
+    Args:
+    -----
+    `args` - Namespace object.\n
+    Return:
+    ------
+    `dict`-  python dict object with details of how to carry out compression proces by means of quantization trick.\n
+    """
+    with open(args.compress) as compress_file:
+        compress_dict = yaml.load(compress_file, Loader=yaml.FullLoader)
+        # pprint(compress_dict)
+    return compress_dict
+
+
+def update_linear_quantizer(a_row: pd.DataFrame, compress_dict: dict, compress_file_path: str) -> None:
+    """Update scheduler for linear quantization.
+    Args:
+    -----
+    `a_row` - pd.DataFrame with details to be exploited for updating data dictionary representing quant options.\n
+    `compress_dict` - dict python object representing quant options.\n
+    `compress_file_path` - str python object, representing dest file path where updated options should be saved in.\n
+    """
+    compress_dict["quantizers"]["linear_quantizer"]["mode"] = a_row["mode"]
+    compress_dict["quantizers"]["linear_quantizer"]["per_channel_wts"] = a_row["per_channel_wts"]
+
+    compress_dict["policies"][0]["starting_epoch"] = a_row["starting_epoch"]
+    compress_dict["policies"][0]["ending_epoch"] = a_row["ending_epoch"]
+    compress_dict["policies"][0]["frequency"] = a_row["frequency"]
+
+    with open(f'{compress_file_path}', 'w') as outfile:
+        yaml.dump(compress_dict, outfile, default_flow_style=False)
+        pass
+    pass
+
+
+def main(args):
+    """Entry point for doing updates required to modify scheduler yaml-like file.
+    Args:
+    -----
+    `args` - Namespace object.\n
+    """
+
+    check_file_exists(args.compress, ".yaml")
+    check_file_exists(args.comb, ".csv")
+
+    df = pd.read_csv(args.comb)
+    if "Unnmaded: 0" in df.columns:
+        df = df.drop(["Unnmaded: 0"], axis = 1)
+
+    if args.pos_comb < 0 or args.pos_comb > df.shape[0]:
+        print(f"Error: pos='{args.pos_comb}' is not allowed since either negative or greater than {df.shape[0]}", file=sys.stderr)
+        sys.exit(-1)
+
+    a_row = df.iloc[args.pos_comb, :]
+    compress_dict = read_yaml_file_to_dict(args)
+
+    if "linear_quantizer" in compress_dict["quantizers"].keys():
+        update_linear_quantizer(a_row, compress_dict, args.compress)
+        pass
+    pass
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    main(args)
+    pass
